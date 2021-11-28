915cc9ebd5098c54a21ee47084f57cacf1e43f3b
jesbivi <jesna1894@gmail.com>
2021-11-28 19:51:34 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,6 @@
+function displayValue(num){
+    let txt_box=document.querySelector("#result")
+    txt_box.value+=num
+}
+var clearBox=(num)=>result.value=""
+var evaluateExp=(num)=>result.value=eval(result.value)
